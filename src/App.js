849eecf20d23b74a81956272dcf53dbb98d393ab
Wafuu <wafuu@wafuu.moe>
2021-07-20 23:43:08 -0700

    Fix wording for credits

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = ({ t }) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/credit" element={<Credit t={t} />} />
+        <Route path="/credits" element={<Credit t={t} />} />
         <Route path="" element={<Nav t={t} />}>
           <Route path="/jp" element={<Main locale="jp" t={t} />} />
           <Route path="/en" element={<Main locale="en" t={t} />} />
